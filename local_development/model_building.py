7c2871a0a26ca71c57de0a77b1ca250880e08352
allanreda <allanreda@hotmail.com>
2025-04-02 23:16:31 +0200

    first commit

---
+++ local_development/model_building.py
@@ -0,0 +1,33 @@
+from google.cloud import bigquery
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/allan/Desktop/Personlige projekter/hyggeskyen_service_account.json'
+
+# Initialize the BigQuery client
+bq_client = bigquery.Client()
+
+# Define SQL query
+query = """
+SELECT 
+      traffic.*,
+      weather.weather_main,
+      weather.weather_description,
+      weather.temperature,
+      weather.feels_like,
+      weather.humidity_percent,
+      weather.visibility,
+      weather.wind_speed,
+      weather.cloudiness_percent
+FROM 
+    `sylvan-mode-413619.copenhagen_data.weather_table` AS weather
+INNER JOIN 
+    `sylvan-mode-413619.copenhagen_data.traffic_table` AS traffic
+ON 
+    weather.geo_name = traffic.geo_name 
+    AND weather.time = traffic.time
+    AND weather.date = traffic.date
+"""
+
+# Run the query and convert to DataFrame
+query_job = bq_client.query(query)
+raw_df = query_job.to_dataframe()
