1a81481fe15c1178f3b4d374d90d18291584915e
allanreda <allanreda@hotmail.com>
2025-07-13 17:37:10 +0200

    gemt

---
+++ local_development/multicollinearity_reduction_class.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+import xgboost as xgb
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from catboost import CatBoostRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+class MulticollinearityReducer:
+    def __init__(self, target_column, corr_threshold=0.7, vif_threshold=10):
+        self.target_column = target_column
+        self.corr_threshold = corr_threshold
+        self.vif_threshold = vif_threshold
+
+    # Function to check for mulitcollinearity and reduce where relevant
+    def reduce_by_correlation(self, df):
+        try:
+            # Drop target column
+            X_df = df.drop(columns=[self.target_column])
+
+            # Generate correlation matrix
+            corr_matrix = X_df.corr(numeric_only=True).abs()
+            # Get the upper triangle of the correlation matrix
+            upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+
+            # Find columns with high correlation
+            to_drop_corr = [column for column in upper.columns if any(upper[column] > self.corr_threshold)]
+
+            # Drop the higly correlated columns
+            df = df.drop(columns=to_drop_corr)
+
+            # Convert all columns to float64
+            df = df.astype('float64')
+
+            logger.info(f"Multicollinearity Reduction: Successfully dropped following columns due to high correlation: {to_drop_corr}")
+            return df
+        
+        except Exception as e:
+            logger.error(f"Multicollinearity Reduction: Error occured when reducing variables by correlation: {e}")
+            return None
+        
+
+    # Function to calculate VIF
+    def calculate_vif(self, df):
+        try:
+            # Drop constant columns (columns with on 0's)
+            df = df.loc[:, df.nunique() > 1]
+            # Create empty dataframe for VIF
+            vif = pd.DataFrame()
+            # Add constant variable to the dataframe
+            df = add_constant(df) 
+            # Get all columns names into VIF table
+            vif["feature"] = df.columns
+            # Calculate VIF for each variable
+            vif["VIF"] = [variance_inflation_factor(df.values, i) for i in range(df.shape[1])]
+            
+            logger.info(f"Multicollinearity Reduction: Successfully calculated VIF")
+            return vif
+        
+        except Exception as e:
+            logger.error(f"Multicollinearity Reduction: Error occured when calculating VIF: {e}")
+            return None
+    
+    # Function to check VIF and drop where relevant
+    def reduce_by_vif(self, df):
+        try:
+        # Drop target column
+        X_df = df.drop(columns=[self.target_column])
+
+        # While loop that keeps running until all the values are below the threshold
+        while True:
+            # Calculate VIF for all the variables
+            vif = self.calculate_vif(X_df)
+            # Drop the const value
+            vif = vif[vif["feature"] != "const"]
+            # Get the variable with the highest VIF
+            max_vif = vif["VIF"].max()
+
+            # If the highest VIF is below the threshold then break
+            if max_vif < self.threshold:
+                break
+            # Else, get the name 
+            feature_to_drop = vif.sort_values("VIF", ascending=False)["feature"].iloc[0]
+            # and drop it from the dataframe
+            try:
+                X_df = X_df.drop(columns=[feature_to_drop])
+                print(f"Dropping '{feature_to_drop}' with VIF = {max_vif:.2f}")
+            except Exception as e:
+                print(f"Could not drop feature in {df} because of the error: {e}")
+        
+        # Merge target column back into the original dataframe
+        X_df[self.target_column] = df[self.target_column]
+        
+        logger.info(f"Multicollinearity Reduction: Successfully dropped following columns due to high correlation: {to_drop_corr}")
+        return X_df
--- local_development/training_pipeline.py
+++ local_development/training_pipeline.py
@@ -10,9 +10,6 @@ bq_client = bigquery.Client()
 # Import OpenWeather API key
 openweather_api_key = Path("C:/Users/allan/Desktop/Personlige projekter/openweather_api_key.txt").read_text()
 
-# Instantiate PreProcessing class
-PreProcessing = PreProcessing(bq_client, openweather_api_key)
-
 # Define SQL query to fetch historical data from Bigquery
 query = """
 SELECT 
@@ -36,3 +33,33 @@ WHERE
     DATE(traffic.date) >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 YEAR)
 """
 
+# Create manual holiday date ranges
+manual_holidays = [
+    ("2024-07-01", "2024-08-09"),
+    ("2024-10-14", "2024-10-18"),
+    ("2024-12-23", "2025-01-02"),
+    ("2025-02-10", "2025-02-14"),
+    ("2025-04-14", "2025-04-21"),
+    ("2025-05-01", "2025-05-01"),
+    ("2025-05-29", "2025-05-30"),
+    ("2025-06-05", "2025-06-05"),
+    ("2025-06-09", "2025-06-09"),
+    ("2025-06-30", "2025-08-08"),
+    ("2025-10-13", "2025-10-17"),
+    ("2025-11-18", "2025-11-18"),
+    ("2025-12-24", "2026-01-02"),
+    ("2026-02-09", "2026-02-13"),
+    ("2026-03-30", "2026-04-06"),
+    ("2026-05-01", "2026-05-01"),
+    ("2026-05-14", "2026-05-15"),
+    ("2026-05-24", "2026-05-25"),
+    ("2026-06-05", "2026-06-05"),
+    ("2026-06-29", "2026-08-10")
+]
+
+# Instantiate PreProcessing class
+PreProcessingClass = PreProcessing(bq_client, openweather_api_key)
+
+raw_df = PreProcessingClass.pull_historical_data(query)
+
+morning_df, afternoon_df = PreProcessingClass.training_preprocessing(raw_df, manual_holidays)
