2112ef11350221bcb1903e9073405d37be888f56
allanreda <allanreda@hotmail.com>
2025-07-20 02:27:33 +0200

    gemt

--- local_development/prediction_pipeline_class.py
+++ local_development/prediction_pipeline_class.py
@@ -29,11 +29,19 @@ class TrainingPipeline:
             logger.error(f"Failed to combine last 8 days data with the 5 day forecast: {e}")
             return None
         
-    def calculate_percentage_diff(predicted, average):
+    def calculate_percentage_diff(self, predicted, average):
         percentage_diff = ((predicted - average) / average) * 100
         return percentage_diff
+    
+    def calculate_percentage_diff_4_days(self, predictions, average):
+        percentage_diff_list = []
+        for prediction in predictions:
+            pred = self.calculate_percentage_diff(prediction, average)
+            percentage_diff_list.append(pred)
+        return percentage_diff_list
 
-    def one_day_load_model_bundle(self, joblib_filename, rush_hour_period):
+    # Function to load elements from joblib created by training pipeline
+    def load_model_bundle(self, joblib_filename, rush_hour_period):
         try:
             # Load joblib file
             prediction_bundle = joblib.load(f"{joblib_filename}.joblib")
@@ -43,8 +51,43 @@ class TrainingPipeline:
             expected_columns = prediction_bundle[f"{rush_hour_period}_columns"]
             avg_traveltime = prediction_bundle[f"{rush_hour_period}_traveltime"]
 
-            logger.info(f"Successfully loaded file {joblib_filename}.joblib and relevant elements")
+            logger.info(f"Successfully loaded file {joblib_filename}.joblib and relevant elements for {rush_hour_period} period")
             return model, scaler, expected_columns, avg_traveltime
         
         except Exception as e:
-            logger.error(f"Error occured when loading file {joblib_filename}.joblib and relevant elements: {e}")
+            logger.error(f"Error occured when loading file {joblib_filename}.joblib and relevant elements for {rush_hour_period} period: {e}")
+            return None, None, None, None
+
+    # Function to predict a rush hour period 
+    def predict(self, X_values, model, scaler, expected_columns, rush_hour_period):
+        try:
+            # Reindex to match the expected column order from training
+            X_aligned = X_values.reindex(columns=expected_columns, fill_value=0)
+            
+            # Scale X columns and predict
+            X_scaled = scaler.transform(X_aligned)
+            prediction = model.predict(X_scaled)
+
+            logger.info(f"Successfully predicted next {rush_hour_period}")
+            return prediction
+        
+        except Exception as e:
+            logger.error(f"Error occured when predicting for next {rush_hour_period}: {e}")
+            return None
+        
+    def predict_next_rush_hour_period(self, joblib_filename, rush_hour_period, X_values)
+        try:
+            logger.info(f"Started prediction pipeline for next {rush_hour_period}.")
+            # Load elements from joblib file
+            model, scaler, expected_columns, avg_traveltime = self.load_model_bundle(joblib_filename, rush_hour_period)
+            # Predict next rush hour period
+            prediction = self.predict(X_values, model, scaler, expected_columns, rush_hour_period)
+            # Calculate how many percent prediction differs from average
+            percentage_diff = self.calculate_percentage_diff(prediction, avg_traveltime)
+            
+            logger.info(f"Successfully finished prediction pipeline for next {rush_hour_period}.")
+            return percentage_diff
+        
+        except Exception as e:
+            logger.error(f"Error occured in prediction pipeline for next {rush_hour_period}: {e}")
+            return None
