7c24f94c4a2d5dbe3af94fc466b75511e605c28e
allanreda <allanreda@hotmail.com>
2025-08-07 23:44:56 +0200

    Removed dead lettering

--- infra/modules/cloud_run_pipeline/main.tf
+++ infra/modules/cloud_run_pipeline/main.tf
@@ -1,75 +1,54 @@
 resource "google_cloud_run_v2_service" "service" {
-  name = var.name
-  project = var.project_id
-  location = var.region
-  deletion_protection = false
-  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+    name = var.name
+    project = var.project_id
+    location = var.region
+    deletion_protection = false
+    ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
 
-  template {
-    service_account = var.service_account_email
-    max_instance_request_concurrency = 1
-
-    containers {
-      image = var.image
-      #image = "gcr.io/google-samples/hello-app:1.0" # Sample image until real deployment
-
-      resources {
-        limits = {
-          cpu = var.cpu
-          memory = var.memory
+    template {
+        service_account = var.service_account_email
+        max_instance_request_concurrency = 1 
+        containers {
+            image = var.image
+            #image = "gcr.io/google-samples/hello-app:1.0" # Sample image until real deployment
+            resources {
+                limits = {
+                cpu = var.cpu
+                memory = var.memory
+                }
+            }
+            env {
+              name  = "MODEL_BUCKET"
+              value = "${var.project_id}-${terraform.workspace}"
+            }
         }
-      }
-
-      env {
-        name = "MODEL_BUCKET"
-        value = "${var.project_id}-${terraform.workspace}"
-      }
+        scaling {
+            min_instance_count = 0
+            max_instance_count = 1
+        }
     }
 
-    scaling {
-      min_instance_count = 0
-      max_instance_count = 1
+    traffic {
+        percent = 100
+        type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
     }
-  }
-
-  traffic {
-    percent = 100
-    type = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-  }
 }
 
 resource "google_pubsub_topic" "topic" {
-  name = "${var.name}-trigger"
+  name    = "${var.name}-trigger"
   project = var.project_id
-}
-
-# Dead-letter topic to avoid retrying failed messages
-resource "google_pubsub_topic" "dead_letter_topic" {
-  name    = "${var.name}-dlq"
-  project = var.project_id
-}
-
-# Grant Pub/Sub permission to publish to the dead letter topic
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
-resource "google_pubsub_topic_iam_member" "allow_dlq_publish" {
-  topic  = google_pubsub_topic.dead_letter_topic.name
-  role   = "roles/pubsub.publisher"
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
 
 resource "google_cloud_scheduler_job" "scheduler" {
-  name      = "${var.name}-scheduler"
-  project   = var.project_id
-  region    = var.region
-  schedule  = var.schedule
-  time_zone = "Europe/Copenhagen"
+  name        = "${var.name}-scheduler"
+  project     = var.project_id
+  region      = var.region
+  schedule    = var.schedule
+  time_zone   = "Europe/Copenhagen"
 
   pubsub_target {
     topic_name = google_pubsub_topic.topic.id
-    data = base64encode("{}")
+    data       = base64encode("{}")
   }
 }
 
@@ -85,10 +64,4 @@ resource "google_pubsub_subscription" "subscription" {
   }
 
   ack_deadline_seconds = 600
-
-  # Only deliver the message once, then send to DLQ
-  dead_letter_policy {
-    dead_letter_topic = google_pubsub_topic.dead_letter_topic.id
-    max_delivery_attempts = 5
-  }
 }
