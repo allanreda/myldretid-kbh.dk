7a83afb9146b86a25a41ed05b7841c769c62a25d
allanreda <allanreda@hotmail.com>
2025-11-04 17:22:47 +0100

    Change values and colors according to new percentage diff calculation

--- website/website/script.js
+++ website/website/script.js
@@ -42,22 +42,22 @@ function formatDateLabel(dateStr, period) {
 }
 
 function getDescription(val) {
-  if (val < -15) return "🚀 Meget hurtigere end normalt";
-  if (val < -10) return "🚗 Hurtigere end normalt";
-  if (val < -5) return "👍 En smule hurtigere end normalt";
-  if (val < 5) return "👌 Omtrent som normalt";
-  if (val < 10) return "⚠️ En smule langsommere end normalt";
-  if (val < 15) return "🛑 Langsommere end normalt";
+  if (val < 15) return "🚀 Meget hurtigere end normalt";
+  if (val < 10) return "🚗 Hurtigere end normalt";
+  if (val < 5) return "👍 En smule hurtigere end normalt";
+  if (val < -5) return "👌 Omtrent som normalt";
+  if (val < -10) return "⚠️ En smule langsommere end normalt";
+  if (val < -15) return "🛑 Langsommere end normalt";
   return "🪦 Meget langsommere end normalt";
 }
 
 function getColor(val) {
-  if (val < -15) return "#05f545";
-  if (val < -10) return "#1abc9c";
-  if (val < -5) return "#37c477";
-  if (val < 5) return "#ffff00";
-  if (val < 10) return "#f39c12";
-  if (val < 15) return "#e67e22";
+  if (val < 15) return "#05f545";
+  if (val < 10) return "#1abc9c";
+  if (val < 5) return "#37c477";
+  if (val < -5) return "#ffff00";
+  if (val < -10) return "#f39c12";
+  if (val < -15) return "#e67e22";
   return "#f70525";
 }
 
